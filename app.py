8751fa479ed8c67df13d7d354a2600506e5ff1a7
maddy <maddy@makeskilled.com>
2023-02-02 10:42:39 +0530

    updating code

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,redirect,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboardpage():
+    return render_template('dashboard.html')
+
+@app.route('/sender')
+def senderpage():
+    return render_template('sender.html')
+
+@app.route('/receiver')
+def receiverpage():
+    return render_template('receiver.html')
+
+@app.route('/logout')
+def logoutpage():
+    return redirect('/')
+    
+if __name__=="__main__":
+    app.run(debug=True)
